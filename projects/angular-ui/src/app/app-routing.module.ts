5dd509faf7ddc50ddeb842a3d969cec5d631814c
Trigve Hagen <trigve.hagen@gmail.com>
2020-04-03 09:45:46 -0700

    bootstrap, navbar, and authentication components

--- projects/angular-ui/src/app/app-routing.module.ts
+++ projects/angular-ui/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginFormComponent } from './authentication/login-form/login-form.component';
+import { RegisterFormComponent } from './authentication/register-form/register-form.component';
+import { ForgotPasswordFormComponent } from './authentication/forgot-password-form/forgot-password-form.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:  "", pathMatch:  "full",redirectTo:  "register"},
+  {path: 'login', component: LoginFormComponent },
+  {path: 'register', component: RegisterFormComponent },
+  {path: 'forgot-password', component: ForgotPasswordFormComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    NgbModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- projects/angular-ui/src/app/app.module.ts
+++ projects/angular-ui/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './authentication/login-form/login-form.component';
+import { RegisterFormComponent } from './authentication/register-form/register-form.component';
+import { ForgotPasswordFormComponent } from './authentication/forgot-password-form/forgot-password-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    ForgotPasswordFormComponent
   ],
   imports: [
     BrowserModule,
